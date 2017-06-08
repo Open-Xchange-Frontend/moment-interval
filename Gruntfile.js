0ff451b0c9a83496f84cf4d1928cdfc52943b6bf
David Bauer <david.bauer@open-xchange.com>
2017-06-09 01:18:11 +0200

    Updated dependencies and packaging files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,74 @@
 var cldr = require('cldr'),
-	fs = require('fs'),
-	path = require('path');
+    fs = require('fs'),
+    path = require('path');
 
 module.exports = function (grunt) {
 
-	grunt.initConfig({
+    grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
     });
 
     var localeJSON = '';
 
-	grunt.registerTask('cldr', 'get cldr data', function(a, b) {
-		var done = this.async();
+    grunt.registerTask('cldr', 'get cldr data', function() {
+        var done = this.async();
 
-		function cldrToMoment(cldrFormat) {
-			return cldrFormat
-				.replace(/('[^']*')|[yad]/g, match => match.length === 1 ? match.toUpperCase(match) : match)
-				.replace(/Y/g, 'YYYY')
-				.replace(/EEEE/g, 'dddd')
-				.replace(/E/g, 'ddd')
-				.replace(/K/g, 'h')
-				.replace(/k/g, 'H')
-				.replace(/'[^']*'/g, match => '[' + match.slice(1,-1) + ']');
-		}
+        function cldrToMoment(cldrFormat) {
+            return cldrFormat
+                .replace(/('[^']*')|[yad]/g, match => match.length === 1 ? match.toUpperCase(match) : match)
+                .replace(/Y/g, 'YYYY')
+                .replace(/EEEE/g, 'dddd')
+                .replace(/E/g, 'ddd')
+                .replace(/K/g, 'h')
+                .replace(/k/g, 'H')
+                .replace(/'[^']*'/g, match => '[' + match.slice(1,-1) + ']');
+        }
 
-		fs.readdir('node_modules/moment/locale', function (err, files) {
-			if (err) throw err;
+        fs.readdir('node_modules/moment/locale', function (err, files) {
+            if (err) throw err;
 
-			var complete = {},
-				fmtstr = [];
+            var complete = {},
+                fmtstr = [];
 
-			// add default language
-			files.push('en.js');
+            // add default language
+            files.push('en.js');
 
-			files.forEach(function (el, i) {
-				var locale = path.basename(el, '.js'),
-					localeCldr = cldr.extractDateIntervalFormats(locale),
-					keys = 'hm,Hm,yMMMd,fallback'.split(',');
-				localeCldr.fallback = cldr.extractDateIntervalFallbackFormat(locale);
+            files.forEach(function (el) {
+                var locale = path.basename(el, '.js'),
+                    localeCldr = cldr.extractDateIntervalFormats(locale),
+                    keys = 'hm,Hm,yMMMd,fallback'.split(',');
+                localeCldr.fallback = cldr.extractDateIntervalFallbackFormat(locale);
 
-				Object.keys(localeCldr).forEach(function (key) {
-					if (keys.indexOf(key) === -1) {
-						delete localeCldr[key];
-					} else {
-						Object.keys(localeCldr[key]).forEach(function (format) {
-							var index = fmtstr.indexOf(localeCldr[key][format]);
-							if (index === -1) {
-								index = fmtstr.push(cldrToMoment(localeCldr[key][format])) - 1;
-							}
-							localeCldr[key][format] = index;
-						});
-					}
-				});
-				var jsonToSting = JSON.stringify(localeCldr, null, 4);
-				complete[locale] = localeCldr;
+                Object.keys(localeCldr).forEach(function (key) {
+                    if (keys.indexOf(key) === -1) {
+                        delete localeCldr[key];
+                    } else {
+                        Object.keys(localeCldr[key]).forEach(function (format) {
+                            var index = fmtstr.indexOf(localeCldr[key][format]);
+                            if (index === -1) {
+                                index = fmtstr.push(cldrToMoment(localeCldr[key][format])) - 1;
+                            }
+                            localeCldr[key][format] = index;
+                        });
+                    }
+                });
+                complete[locale] = localeCldr;
 
-			});
+            });
 
-			// add format strings to locales object
-			complete.formats = fmtstr;
-			localeJSON = JSON.stringify(complete, null, 0);
+            // add format strings to locales object
+            complete.formats = fmtstr;
+            localeJSON = JSON.stringify(complete, null, 0);
 
-			done();
-		});
-	});
+            done();
+        });
+    });
 
     grunt.config.merge({
         assemble: {
             options: {
                 locale: function () {
-                	return localeJSON;
+                    return localeJSON;
                 }
             },
             interval: {
@@ -86,16 +85,20 @@ module.exports = function (grunt) {
             }
         },
         uglify: {
-    		my_target: {
-		        files: {
-		        	'moment-interval.min.js': ['moment-interval.js']
-      			}
-    		}
-  		}
+            my_target: {
+                files: {
+                    'moment-interval.min.js': ['moment-interval.js']
+                }
+            }
+        },
+        eslint: {
+            target: ['Gruntfile.js', 'moment-interval.js']
+        }
     });
 
-	grunt.loadNpmTasks('assemble');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-assemble');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', [ 'cldr', 'assemble:interval', 'uglify' ]);
+    grunt.registerTask('default', [ 'cldr', 'assemble:interval', 'eslint', 'uglify' ]);
 };
